1f923b5cd0340183b8bfc1801fe002603a441ea8
jnair559 <47804021+jnair559@users.noreply.github.com>
2019-02-22 10:55:20 +0530

    Create testing.c

---
+++ testing.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+    int i;
+    int*p;
+    int j=20;
+    int a,b,sum;
+    a=10;
+    b=40;
+    sum = a+b;
+    printf("sum =%d",sum);
+}
